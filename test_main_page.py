4d98bd19901455d96933495ba8b7a9765d7cbea0
MStryapchiy <MustStr@gmail.com>
2023-02-12 23:24:59 +0400

    add base page and main page, add test for open login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,10 @@
-from selenium.webdriver.common.by import By
-import time
-
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.main_page import MainPage
 
 
-class TestItems():
-    def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()   
 
 
-# link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-
-
-# class TestItems():
-#     def test_search_for_add_to_bascket_button(self, browser):
-#         browser.get(link)
-#         browser.implicitly_wait(5)
-#         assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-#         time.sleep(30)
